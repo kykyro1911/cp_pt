15337edec2291f122f0389414a7c6b5cd2e81a72
Nono Wang <nono.wang@intech.gwdomain.24k.local>
2020-07-01 09:21:30 +0800

    1

---
+++ admin.bat
@@ -0,0 +1,18 @@
+@echo off
+echo 獲取Administrator權限
+cacls.exe "%SystemDrive%\System Volume Information" >nul 2>nul
+if %errorlevel%==0 goto Admin
+if exist "%temp%\getadmin.vbs" del /f /q "%temp%\getadmin.vbs"
+echo Set RequestUAC = CreateObject^("Shell.Application"^)>"%temp%\getadmin.vbs"
+echo RequestUAC.ShellExecute "%~s0","","","runas",1 >>"%temp%\getadmin.vbs"
+echo WScript.Quit >>"%temp%\getadmin.vbs"
+"%temp%\getadmin.vbs" /f
+if exist "%temp%\getadmin.vbs" del /f /q "%temp%\getadmin.vbs"
+exit
+
+:Admin
+echo 成功取得Administrator权限
+————————————————
+
+
+rem netsh interface set interface name=”乙太網路” admin=enable
